c2d3733424be5a355372fd8968c1b49af567f114
pengxs42098 <pengxs42098@hundsun.com>
2023-04-04 16:18:24 +0800

    Initial commit

---
+++ src/main/java/com/qianfeng/magic/home/MagicHomeApplication.java
@@ -0,0 +1,13 @@
+package com.qianfeng.magic.home;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagicHomeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MagicHomeApplication.class, args);
+    }
+
+}
